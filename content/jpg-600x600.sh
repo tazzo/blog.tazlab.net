1cc70d18ede9e3e57a2bd8004fc9d80432451566
tazzo <roberto.tazzoli@gmail.com>
2026-01-11 01:28:51 +0100

    chore: update featured images and convert png to jpg

--- content/jpg-600x600.sh
+++ content/jpg-600x600.sh
@@ -11,6 +11,6 @@ echo "Ridimensionamento ricorsivo delle immagini JPG a max 1024x1024..."
 # Trova tutti i file .jpg o .jpeg ricorsivamente
 # -resize "1024x1024>" indica a ImageMagick di ridimensionare SOLO se
 # l'immagine è più grande di queste dimensioni, mantenendo le proporzioni.
-find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 mogrify -resize "1024x1024>"
+find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 mogrify -resize "600x600>"
 
 echo "Ottimizzazione completata! I file più grandi di 1024px sono stati sovrascritti."
